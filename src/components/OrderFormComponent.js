008f245b168eca680655f7ad100bb81fd117c4df
ChuckHagy <chuckhagy@gmail.com>
2017-08-23 07:06:38 -0700

    responsive styling

--- src/components/OrderFormComponent.js
+++ src/components/OrderFormComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function OrderFormComponent({ defaultCustomerInfo }){
   return(
-    <div className="container">
+    <div className="container OrderFormComponent">
       <form>
       <div className="row">
           <div className="col l2 s3">
