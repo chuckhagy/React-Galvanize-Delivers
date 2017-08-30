6f2cfe83a759fe76fcf1ac7ed0bda67d7f233c97
ChuckHagy <chuckhagy@gmail.com>
2017-08-30 16:02:58 -0700

    refactored to appjs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import OrderPage from './components/OrderPage'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+let menuItems = [
+  {
+    id: '001',
+    name: 'Some Item A',
+    price: 9.99,
+    imagePath: '//via.placeholder.com/300x200'
+  },
+  {
+    id: '002',
+    name: 'Some Item B',
+    price: 8.99,
+    imagePath: '//via.placeholder.com/300x200'
+  },
+  {
+    id: '003',
+    name: 'Some Item C',
+    price: 7.99,
+    imagePath: '//via.placeholder.com/300x200'
+  },
+  {
+    id: '004',
+    name: 'Some Item D',
+    price: 6.99,
+    imagePath: '//via.placeholder.com/300x200'
   }
-}
+]
+export default class App extends Component {
+  state={
+    menuItems: menuItems,
+    orderItems: [],
+    customerInfo: null
+  }
 
-export default App;
+  render() {
+    return(
+      <div className="App">
+        <OrderPage
+          menuItems={this.state.menuItems}
+          orderItems={this.state.orderItems}
+          customerInfo={this.state.customerInfo}
+          onAddItem={this._onAddItem}
+          onSubmitOrderForm={this._onSubmitOrderForm}
+          onCloseOrderSuccessMessage={this._onCloseOrderSuccessMessage}
+        />
+      </div>
+    )
+  }
+  _onAddItem = itemId => {
+    this.setState(prevState => {
+      return{
+        orderItems: [...prevState.orderItems, prevState.menuItems.find(item => item.id === itemId)]
+      }
+    })
+  };
+
+  _onSubmitOrderForm = ({name, phone, address}) => {
+    this.setState({customerInfo: {name, phone, address}});
+  }
+
+  _onCloseOrderSuccessMessage = event => {
+    this.setState({customerInfo: null});
+    this.setState({orderItems: []});
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,65 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import OrderPage from './components/OrderPage'
+import App from './App'
 
-let menuItems = [
-  {
-    id: '001',
-    name: 'Some Item A',
-    price: 9.99,
-    imagePath: '//via.placeholder.com/300x200'
-  },
-  {
-    id: '002',
-    name: 'Some Item B',
-    price: 8.99,
-    imagePath: '//via.placeholder.com/300x200'
-  },
-  {
-    id: '003',
-    name: 'Some Item C',
-    price: 7.99,
-    imagePath: '//via.placeholder.com/300x200'
-  },
-  {
-    id: '004',
-    name: 'Some Item D',
-    price: 6.99,
-    imagePath: '//via.placeholder.com/300x200'
-  }
-]
-
-let orderItems = [];
-
-let customerInfo=null;
-
-render();
-
-function onAddItem(itemId){
-  orderItems.push(menuItems.find(item => item.id === itemId));
-  render();
-};
-
-function onSubmitOrderForm({name, phone, address}){
-  customerInfo={name, phone, address};
-  render();
-}
-
-function onCloseOrderSuccessMessage(){
-  customerInfo=null;
-  orderItems = [];
-
-  render();
-}
-
-function render(){
-ReactDOM.render(<OrderPage
-  menuItems={menuItems}
-  orderItems={orderItems}
-  customerInfo={customerInfo}
-  onAddItem={onAddItem}
-  onSubmitOrderForm={onSubmitOrderForm}
-  onCloseOrderSuccessMessage={onCloseOrderSuccessMessage}
-/>, document.getElementById('root'));
-}
+ReactDOM.render(<App />, document.getElementById('root'));
