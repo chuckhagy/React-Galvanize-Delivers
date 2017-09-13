284b2bb93db2d721319b4fe746eff0b0bd33d303
ChuckHagy <chuckhagy@gmail.com>
2017-09-13 14:59:26 -0700

    going to 4aa

--- src/redux/setupStore.js
+++ src/redux/setupStore.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './rootReducer';
+import thunkMiddleware from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancers = composeEnhancers(applyMiddleware(thunkMiddleware));
 
 export default function (){
-  return createStore(rootReducer);
+  return createStore(rootReducer, enhancers);
 }
